5b27606a1ed118f2ad4de00a982f5eea3d9cbe24
Javitocatral <javitocatral@gmail.com>
2023-11-15 16:46:50 +0100

    first page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="app">
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<dir class="container">
+  <div class="iamge">
+    <img
+      src="https://c.media.kavehome.com/images/Ambiences/A000001980_3.jpg?tx=w_900,c_fill,ar_0.8,g_auto"
+      alt="salon"
+    />
+    <div class="header">
+      <div class="logo"><h3>María la torre</h3></div>
+
+      <div class="button"><p>Quien soy</p></div>
+      <div class="button"><p>Servicios</p></div>
+      <div class="button"><p>Contacto</p></div>
+    </div>
+    <div class="text">
+      <h1>Tu espacio Trabaja para ti</h1>
+      <p>
+        Tanto tu hogar como tu espacio de trabajo puede ofrecerte la suerte que
+        necesitas o todo lo contrario, yo te ayudo a conocerlo y armonizarlo
+      </p>
+    </div>
+  </div>
+</dir>
+
+<style>
+  .container {
+    position: relative;
+  }
+  .button {
+    width: 20;
+    background-color: #fd9c9b;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 60px 20px;
+  }
+  .logo {
+    width: 25%;
+    background-color: azure;
+    padding: 40px;
+    margin: 25px;
+    margin-right: 50px;
+  }
+  .header {
+    display: flex;
+  }
+  h1 {
+    font-size: 100px;
+    z-index: 10;
+    padding-right: 628px;
+    padding-left: 80px;
+  }
+  p {
+    z-index: 10;
+    padding-left: 80px;
+    font-size: 35px;
+    padding-right: 628px;
+  }
+
+  img {
+    display: flex;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+</style>
